4c82df08f2db6524e236d17842100e7854d43fcf
Eunice Mojolaoluwa Oduwole <jolaoduwole@gmail.com>
2022-04-26 20:26:16 +0300

    updated codes

--- CLI_eunice.py
+++ CLI_eunice.py
@@ -6,17 +6,17 @@ import argparse
 """ 
 HOW TO USE:
 1. Open your terminal and set the working directory where the
-heart_attack_pred.py file is located.
+cli.py file is located.
 Example:
 cd /d E:\strive_school\work\CHALLENGE\Chap2\Hipocratia
 
-2. Using python command, run the heart_attack_pred.py file followed
+2. Using python command, run the cli.py file followed
 csv file where the records are stored.
 Example:
-python heart_attack_pred.py device/example_measurements.csv
+python cli.py device/test_data.csv
 
 
-NOTE: csv file must followed the same structure as `example_measurements.csv`.
+NOTE: csv file must followed the same structure as `test_data.csv`.
 It's the same structure as `X_test`
 """
 
@@ -33,10 +33,11 @@ parser.add_argument('file_path', type=str,
 args = parser.parse_args()
 
 path_to_records = args.file_path
+# print(args)
 
 # Loading records
 user_data = pd.read_csv(path_to_records)
-column_names = user_data.columns
+# column_names = user_data.columns
 
 # Predictions
 for _, row in user_data.iterrows():
@@ -45,6 +46,7 @@ for _, row in user_data.iterrows():
     print(x.to_string(index=False), '\n')
 
     pred = model.predict(x)[0]
+    # print(pred)
     if pred == 1:
         print("You are prone to heart attack...Please take your drugs!!!")
     else:
--- heart_attack.py
+++ heart_attack.py
@@ -1,91 +1,103 @@
-from ast import Or
-from multiprocessing import Pipe
-import numpy as np
 import pandas as pd
-from scipy import rand
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+import numpy as np
 import time
-from sklearn.pipeline import Pipeline, make_pipeline
-from sklearn.metrics import accuracy_score, balanced_accuracy_score, plot_confusion_matrix,classification_report
-import matplotlib.pyplot as plt
+import joblib
 
-from sklearn.tree import DecisionTreeClassifier
+# Training Test
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import accuracy_score, plot_confusion_matrix, classification_report
+
+# Algorithms
+from sklearn.ensemble      import RandomForestClassifier
+from sklearn.ensemble      import ExtraTreesClassifier
+from sklearn.ensemble      import AdaBoostClassifier
+from sklearn.ensemble      import GradientBoostingClassifier
 from sklearn.ensemble      import HistGradientBoostingClassifier
-from xgboost import XGBClassifier
-from lightgbm import LGBMClassifier
-from catboost import CatBoostClassifier
-from sklearn.linear_model import LogisticRegression
+from xgboost               import XGBClassifier
+from lightgbm              import LGBMClassifier
+from catboost              import CatBoostClassifier
 
 # Load and Prepro 
 
 # Load data
-data = pd.read_csv('data/heart.csv')
+def load_and_cleaning(filepath):
+    raw_data = pd.read_csv(filepath)
+    clean_data = raw_data.drop_duplicates()
+    return clean_data
 
-# remove duplicates
-data = data.drop_duplicates()
 # print('Shape of data after dropping duplicate',data.shape)
 
 # Train test sets
-X = data.drop(columns='output')
-y = data['output']
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=0)
-print('Xtrain shape', X_train.shape)
-print('ytrain shape', y_train.shape)
-print('Xtest shape', X_test.shape)
-print('ytest shape', y_test.shape)
+def splitting_data(data):
+    X = data.drop(columns='output')
+    y = data['output']
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=0)
+    # print('Xtrain shape', X_train.shape)
+    # print('ytrain shape', y_train.shape)
+    # print('Xtest shape', X_test.shape)
+    # print('ytest shape', y_test.shape)
+    return X_train, X_test, y_train, y_test
+
+
 
 # Preprocessor
-num_var = ['age', 'trtbps', 'chol', 'thalachh', 'oldpeak']
+
+def data_preprocessing(numerical_variables):
+    # num_var = ['age', 'trtbps', 'chol', 'thalachh', 'oldpeak']
+    
 
 
-num_prep = ColumnTransformer([('num_prepo', StandardScaler(), num_var)],
-                             remainder='passthrough')
+    num_prep = ColumnTransformer([('num_prepo', StandardScaler(), numerical_variables)],
+                                remainder='passthrough')
 
 
-tree_classifiers = {
-  "Extra Trees": ExtraTreesClassifier(random_state=0),
-  "Random Forest":RandomForestClassifier(random_state=0),
-  "AdaBoost": AdaBoostClassifier(random_state=0),
-  "Skl GBM": GradientBoostingClassifier(random_state=0),
-  "Skl HistGBM": HistGradientBoostingClassifier(random_state=0),
-  "XGBoost": XGBClassifier(),
-  "LightGBM": LGBMClassifier(random_state=0),
-  "CatBoost": CatBoostClassifier(random_state=0)}
+    tree_classifiers = {
+    "Extra Trees": ExtraTreesClassifier(random_state=0),
+    "Random Forest":RandomForestClassifier(random_state=0),
+    "AdaBoost": AdaBoostClassifier(random_state=0),
+    "Skl GBM": GradientBoostingClassifier(random_state=0),
+    "Skl HistGBM": HistGradientBoostingClassifier(random_state=0),
+    "XGBoost": XGBClassifier(),
+    "LightGBM": LGBMClassifier(random_state=0),
+    "CatBoost": CatBoostClassifier(random_state=0)}
 
-tree_classifiers_pipe = {name: make_pipeline(num_prep, model) for name, model in tree_classifiers.items()}
+    tree_classifiers_pipe = {name: make_pipeline(num_prep, model) for name, model in tree_classifiers.items()}
+    return tree_classifiers_pipe
+
 
 # Benchmark accuracy
-# results = pd.DataFrame({'Model': [], 'Accuracy': [], 'Time': []})
 
-# for model_name, model in tree_classifiers_pipe.items():
-#     start_time = time.time()
-    
-#     # Training
-#     model.fit(X_train, y_train)
-    
-#     # Prediction
-#     pred = model.predict(X_test)
+def model_perfromance(classifiers,X_train,y_train,X_test,y_test):   
 
-#     total_time = time.time() - start_time # Time taken to fit and predict
+    results = pd.DataFrame({'Model': [], 'Accuracy': [], 'Time': []})
 
-#     model_results = pd.DataFrame({"Model":    [model_name],
-#                               "Accuracy": [accuracy_score(y_test, pred)*100],
-#                               "Time":     [total_time]})
-#     results = pd.concat([results, model_results])
-    
-    
+    for model_name, model in classifiers.items():
+        start_time = time.time()
+        
+        # Training
+        model.fit(X_train, y_train)
+        
+        # Prediction
+        pred = model.predict(X_test)
 
-# results_ord = results.sort_values(by=['Accuracy'], ascending=False, ignore_index=True)
-# print('Accuracy and timing of models \n',results_ord)
+        total_time = time.time() - start_time # Time taken to fit and predict
+
+        model_results = pd.DataFrame({"Model":    [model_name],
+                                "Accuracy": [accuracy_score(y_test, pred)*100],
+                                "Time":     [total_time]})
+        results = pd.concat([results, model_results])
+        
+        
+
+    results_ord = results.sort_values(by=['Accuracy'], ascending=False, ignore_index=True)
+    print('Accuracy and timing of models \n',results_ord)
 
 
 """
@@ -102,7 +114,7 @@ tree_classifiers_pipe = {name: make_pipeline(num_prep, model) for name, model in
 """
 
 
-def data_enhancement(data):
+def generate_data(data):
     np.random.seed(0)
     gen_data = data.copy()
     sep_on = 'sex'
@@ -141,58 +153,28 @@ def data_enhancement(data):
 
  
 
-def Augmented_data():
-        
-    gen = data_enhancement(data)
+def augmented_data(generated_data,X_train,y_train):
     np.random.seed(0)
+    gen = generated_data
     extra_data = gen.sample(gen.shape[0] // 5)
     X_train_aug = pd.concat([X_train, extra_data.drop(['output'], axis=1) ])
     y_train_aug = pd.concat([y_train, extra_data['output'] ])
     # print(f'Augmented X_train by {((len(X_train_aug) - len(X_train)) / len(X_train)) * 100 }%')
     return X_train_aug, y_train_aug
 
-def new_model():
-    Xtrain_aug, ytrain_aug = Augmented_data()
-    tree_classifiers_pipe_augg = {names: make_pipeline(num_prep, models) for names, models in tree_classifiers.items()}
-    results_aug = pd.DataFrame({'Models': [], 'Accuracies': [], 'Timing': []})
-
-    for model_names1, models1 in tree_classifiers_pipe_augg.items():
-        start_time = time.time()
-        
-        # Training
-        models1.fit(Xtrain_aug, ytrain_aug)
-        
-        # Prediction
-        pred_aug = models1.predict(X_test)
-
-        total_time = time.time() - start_time # Time taken to fit and predict
-
-        model_results_aug = pd.DataFrame({"Models":    [model_names1],
-                                "Accuracies": [accuracy_score(y_test, pred_aug)*100],
-                                "Timing":     [total_time]})
-
-
-        results_aug = pd.concat([results_aug, model_results_aug])
-        results_ord_aug = results_aug.sort_values(by=['Accuracies'], ascending=False, ignore_index=True)
-    print(results_ord_aug)
-    return tree_classifiers_pipe_augg   
-            
+   
+     
 
   
-def final_model():
-    final_tree_classifiers_pipe_augg = new_model()
-    Xtrain_final, ytrain_final= Augmented_data()
-    final_mod = final_tree_classifiers_pipe_augg['Extra Trees']
+def final_model(classifier,model_name,X_train_aug, y_train_aug):
 
-    final_mod.fit(Xtrain_final, ytrain_final)
-
-    pred_final = final_mod.predict(X_test)
-    # print(classification_report(y_test, pred_final))
-    plot_confusion_matrix(final_mod, X_test, y_test)
+    final_model = classifier[model_name]
+    final_model.fit(X_train_aug,y_train_aug)
+    return final_model 
 
 
     
-final_model()   
+
 
     
     
